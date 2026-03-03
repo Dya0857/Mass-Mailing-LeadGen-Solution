6ea453ce7ad92bbd659856ffaf235f78a09f299a
D_ya0857 <144965931+Dya0857@users.noreply.github.com>
2026-03-03 18:15:58 +0530

    Configure Vite server and preview settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  preview: {
+    port: 3000,
+    host: '0.0.0.0',
+    allowedHosts: ['mass-mailing-solution-frontend-1.onrender.com', 'mass-mailing-frontend.onrender.com', 'localhost', '127.0.0.1'],
+  }
 })
